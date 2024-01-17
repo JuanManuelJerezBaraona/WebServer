a37f58fd7be095598f870d5a1d31cb30051a0612
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-17 12:15:24 -0300

    Update .gitignore and app.js, and modify start script in package.json

---
+++ api/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Juan Jerez',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Juan Jerez',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Juan Jerez',
+        titulo: 'Curso de Node'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.send('404 | Page not found');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+module.exports = app;
